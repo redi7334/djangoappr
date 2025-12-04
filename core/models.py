68cd31a0b9538f2e03b39bba840495cd19c7a57d
unknown <redimema2211@gmail.com>
2025-12-04 20:51:51 +0100

    New model db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class StudySession(models.Model):
+    Subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration_minutes = models.IntegerField()
+    notes = models.TextField(blank=True)
