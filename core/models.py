f7ab29fea6e7696b4b44e2e7a940baeb2bbe4fd2
unknown <mattias00@live.com>
2025-12-04 20:53:44 +0100

    New models for db, migrations

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ class Subject(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class StudySession(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     date = models.DateField()
