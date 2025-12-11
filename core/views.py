16e91b4e336df0cbb4d30a409a14e22fff8eddb7
unknown <mattias00@live.com>
2025-12-11 20:45:37 +0100

    Added new POST endpoint

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Subject
-
-# Create your views here.
+import json
 
 def test_view(request):
     return JsonResponse({"message":"view is working."})
@@ -14,6 +14,7 @@ def subject_list(request):
         return JsonResponse(subjects, safe=False)
     return JsonResponse({"Error":"Method not allowed."})
 
+@csrf_exempt
 def subject(request, numri):
     if request.method == "GET":
         subject = Subject.objects.get(id=numri)
@@ -25,4 +26,31 @@ def subject(request, numri):
         }
 
         return JsonResponse(subject_dict, safe=False)
+    if request.method == "POST":
+        # Duhen marre te dhenat nga requesti
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+        
+        name = data.get("name")
+        description = data.get("description", "")
+
+        # Duhet krijuar objekti ne db
+        subject = Subject.objects.create(name=name, description=description)
+
+        # Dergo mesazh suksesi
+        return JsonResponse({"message":"Subject was created succesfully"})
+    
+    if request.method == "PATCH":
+        ...
+        # Marrim te dhenat nga requesti (id, name, description)
+
+        # Duhet marr objekti nga db me id
+        # Duhet ndryshuar name dhe description
+        # Duhet ber save objekti ne db
+
+        # return successful message, Old dhe new
+
+
     return JsonResponse({"Error":"Method not allowed."})
